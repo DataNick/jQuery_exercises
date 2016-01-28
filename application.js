c3e2a56bbdebf028755fa3bcba77e12f12ad0650
DataNick <DataNick@users.noreply.github.com>
2016-01-28 18:00:56 -0500

    Removed inline javascript from html and placed inside application.js file

--- application.js
+++ application.js
@@ -1,1 +1,13 @@
-application.js
+$(document).ready(function(){
+  x = 0;
+  $("div").scroll(function(){
+    $("span").text( x+= 1);
+  });
+
+});
+
+// descendant selector
+// $(".destination li")
+
+// child selector
+// $(".destination > li")
