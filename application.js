94783a106860af477c0e3c12cfe71239699cacf9
DataNick <DataNick@users.noreply.github.com>
2016-01-28 21:14:26 -0500

    Added notes and click feature to page to retrieve price

--- application.js
+++ application.js
@@ -4,10 +4,41 @@ $(document).ready(function(){
     $("span").text( x+= 1);
   });
 
+  //create a <p> node with a price
+  //Create a new DOM node by sending the new html into jQuery function
+  var price = $("<p>From $399.99</p>"); //creates new node BUT doesn't add to DOM
+  //Ways to add node to DOM
+  //1. append(<element>);
+  //2. prepend(<element>);
+  //3. after(<element>);
+  //4. before(<element>);
+  //1. appendTo(<element>);
+  //2. prependTo(<element>);
+  //3. insertAfter(<element>);
+  //4. insertBefore(<element>);
+
+
+  //select node of out DOM to serve as reference point to put price node before
+  $('button').on('click', function(){
+    (price).prependTo($("#intro"));
+    $('button').remove();
+
+  });
+
+
 });
 
 // descendant selector
-// $(".destination li")
+// $(".destination li");
 
-// child selector
-// $(".destination > li")
+// child selector or direct descendant
+// $(".destination > li");
+// find li elements that are children of .destination
+
+//selecting multiple elements
+// $(".promo, #france");
+
+//Manipulating the DOM
+//Interaction]
+//$(document).ready(<event handler function>);
+//create a function
